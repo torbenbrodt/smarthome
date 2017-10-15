1de38ca942a3642add8ece80e82e7041ae76962e
Torben Brodt <t.brodt@gmail.com>
2017-10-15 19:53:19 +0200

    Add config parameters to Motion Script

--- motion/pir.py
+++ motion/pir.py
@@ -1,8 +1,22 @@
+from ConfigParser import ConfigParser
+from os.path import expanduser
 import RPi.GPIO as GPIO
 import time
 import requests
+import argparse
  
-SENSOR_PIN = 23
+parser = argparse.ArgumentParser(description='Chromecast toolset.')
+parser.add_argument('--sensorpin', help='Sensor Pin.')
+args = parser.parse_args()
+
+config = ConfigParser()
+config.read(expanduser('~/assistant-helper/smarthome.ini'))
+
+TRANSMITTER_URL = config.get('transmitter', 'api_url')
+TRANSMITTER_USER = config.get('transmitter', 'api_user')
+TRANSMITTER_PASS = config.get('transmitter', 'api_pass')
+
+SENSOR_PIN = args.sensorpin
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
@@ -12,7 +26,7 @@ def mein_callback(channel):
         switch = "an"
     else:
         switch = "aus"
-    r = requests.post("http://localhost:9900/transmitter?device=ecklampe&switch=" + switch, auth=('apiai', 'XXX'))
+    r = requests.post(TRANSMITTER_URL + "/transmitter?device=ecklampe&switch=" + switch, auth=(TRANSMITTER_USER, TRANSMITTER_PASS))
 
 try:
     GPIO.add_event_detect(SENSOR_PIN , GPIO.BOTH, callback=mein_callback)
